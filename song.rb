f660012baadb75ae6c35b9d2d17bf681c6dd929f
Katrina Owen <katrina.owen@gmail.com>
2016-03-28 17:26:35 -0600

    Fix chain to handle the wriggly bit of the spider
    Discovered when using the chain that the spider was different.
    This led to the discovery of the Critter object.

--- song.rb
+++ song.rb
@@ -1,4 +1,23 @@
+class Critter < Struct.new(:name, :qualifier)
+end
+
 class Song
+  DATA = [
+    ["horse"],
+    ["cow"],
+    ["goat"],
+    ["dog"],
+    ["cat"],
+    ["bird"],
+    ["spider", "that wriggled and jiggled and tickled inside her"],
+    ["fly"],
+  ]
+
+  attr_reader :critters
+  def initialize
+    @critters = DATA.map {|row| Critter.new(*row)}
+  end
+
   def lyrics
     (1..8).map {|i| verse(i)}.join("\n")
   end
@@ -61,18 +80,18 @@ class Song
     end
   end
 
-  def chain
-    [
-      "cow", "goat", "dog", "cat", "bird", "spider", "fly"
-    ].each_cons(2).map {|pair|
+  def chain(i)
+    critters.last(i).each_cons(2).map {|pair|
       motivation(*pair)
     }.join("\n")
   end
 
   def motivation(predator, prey)
     "She swallowed the %s to catch the %s." % [
-      predator,
-      prey,
+      predator.name,
+      [
+        prey.name, prey.qualifier
+      ].compact.join(" ")
     ]
   end
 end
